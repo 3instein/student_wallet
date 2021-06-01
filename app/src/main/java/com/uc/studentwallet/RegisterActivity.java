c207c932d588bb5f1e2daf542f443593a95e1e7a
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2021-06-01 15:03:17 +0700

    Connection to database

--- app/src/main/java/com/uc/studentwallet/RegisterActivity.java
+++ app/src/main/java/com/uc/studentwallet/RegisterActivity.java
@@ -6,12 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import model.User;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
     Intent intent;
     TextView register_login_text;
+    TextInputLayout register_input_username, register_input_password;
+    Button register_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +38,23 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         initComponent();
         loginExistingUserBtn();
+
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = register_input_username.getEditText().getText().toString().trim();
+                String password = register_input_password.getEditText().getText().toString().trim();
+                User newUser = new User(username, password);
+                register(newUser);
+            }
+        });
     }
 
     private void initComponent() {
-        register_login_text = (TextView) findViewById(R.id.register_login_text);
+        register_login_text = findViewById(R.id.register_login_text);
+        register_input_username = findViewById(R.id.register_input_username);
+        register_input_password = findViewById(R.id.register_input_password);
+        register_btn = findViewById(R.id.register_btn);
     }
 
     private void loginExistingUserBtn() {
@@ -36,5 +68,33 @@ public class RegisterActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void register(User user) {
+        String url = "http://192.168.0.176/student_wallet/register.php";
+        RequestQueue requestQueue = Volley.newRequestQueue(getBaseContext());
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getBaseContext(), "Success!", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getBaseContext(), "Failed!" + error, Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> data = new HashMap<>();
+                data.put("username", user.getUsername());
+                data.put("password", user.getPassword());
+
+                return data;
+            }
+        };
+
+        requestQueue.add(stringRequest);
     }
 }
