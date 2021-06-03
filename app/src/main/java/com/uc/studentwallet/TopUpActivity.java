101942dea00e187d7af1f4df7d23f8990e765671
SsAbrut <michael.gunawan2002@gmail.com>
2021-06-03 08:35:18 +0700

    Added Top Up Page

--- app/src/main/java/com/uc/studentwallet/TopUpActivity.java
+++ app/src/main/java/com/uc/studentwallet/TopUpActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CheckBalanceActivity extends AppCompatActivity {
+public class TopUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_check_balance);
+        setContentView(R.layout.activity_top_up);
     }
 }
