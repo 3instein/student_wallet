9777bb2e6edd961cef53cf611eee8ed9dab9aacb
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2021-06-04 16:23:01 +0700

    Updated ForgotActivity and ForgotActivity2.java

--- app/src/main/java/com/uc/studentwallet/ForgotActivity.java
+++ app/src/main/java/com/uc/studentwallet/ForgotActivity.java
@@ -4,10 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ForgotActivity extends AppCompatActivity {
 
@@ -24,14 +37,54 @@ public class ForgotActivity extends AppCompatActivity {
         forgot1_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent = new Intent(getBaseContext(), ForgotActivity2.class);
-                startActivity(intent);
+                String username = forgot_username.getEditText().getText().toString().trim();
+                getToken(username);
             }
         });
+
+
     }
 
     private void initComponent() {
         forgot1_btn = findViewById(R.id.forgot1_btn);
         forgot_username = findViewById(R.id.forgot_username);
     }
+
+    private void getToken(String username){
+        String url = "https://student.hackerexperience.net/forgot.php";
+        RequestQueue requestQueue = Volley.newRequestQueue(getBaseContext());
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.equalsIgnoreCase("success")) {
+                    intent = new Intent(getBaseContext(), ForgotActivity2.class);
+                    startActivity(intent);
+                    finish();
+                } else if(response.equalsIgnoreCase("no data")){
+                    Toast.makeText(getBaseContext(), "Invalid Username", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getBaseContext(), "" + error, Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> data = new HashMap<>();
+                data.put("username", username);
+
+                return data;
+            }
+        };
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                0,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        requestQueue.add(stringRequest);
+
+    }
+
 }
--- app/src/main/java/com/uc/studentwallet/ForgotActivity2.java
+++ app/src/main/java/com/uc/studentwallet/ForgotActivity2.java
@@ -2,10 +2,24 @@ package com.uc.studentwallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ForgotActivity2 extends AppCompatActivity {
 
@@ -17,6 +31,25 @@ public class ForgotActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot2);
         initComponent();
+
+        forgot2_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), ForgotActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        forgot2_reset_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String token = forgot2_token.getEditText().getText().toString().trim();
+                String new_password = forgot2_new_password.getEditText().getText().toString().trim();
+
+                resetPassword(token, new_password);
+            }
+        });
     }
 
     private void initComponent() {
@@ -24,5 +57,42 @@ public class ForgotActivity2 extends AppCompatActivity {
         forgot2_new_password = findViewById(R.id.forgot2_new_password);
         forgot2_back = findViewById(R.id.forgot2_back);
         forgot2_reset_password = findViewById(R.id.forgot2_reset_password);
+    }
+
+    public void resetPassword(String token, String new_password){
+        String url = "https://student.hackerexperience.net/forgot.php";
+        RequestQueue requestQueue = Volley.newRequestQueue(getBaseContext());
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.equalsIgnoreCase("success")) {
+                    Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else if(response.equalsIgnoreCase("invalid")){
+                    Toast.makeText(getBaseContext(), "Invalid Token", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getBaseContext(), "" + error, Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> data = new HashMap<>();
+                data.put("token", token);
+                data.put("new_password", new_password);
+
+                return data;
+            }
+        };
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                0,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        requestQueue.add(stringRequest);
     }
 }
